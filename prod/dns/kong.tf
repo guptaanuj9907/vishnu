da29214ae3ee8cd90d19b99992eed79b9c0e1e7d
anuj.gupta <anuj.gupta@razorpay.com>
2025-04-03 10:49:25 +0550

    Added dns entry..

--- prod/dns/kong.tf
+++ prod/dns/kong.tf
@@ -47,3 +47,27 @@ module "my-app-int" {
   setid         = ['prod-green-tv2', 'prod-white-tv2']
   weight_record = ['prod-green-int-v2.razorpay.com', 'prod-white-int-v2.razorpay.com']
 }
+module "my-app" {
+  source        = "../../modules/weighted-dns/"
+  zone_id       = data.aws_route53_zone.com.id
+  record_name   = "edge"
+  record_type   = "CNAME"
+  env           = "cde"
+  ttl           = "300"
+  target_record = "cde-edge-weighted.rzp.com"
+  weight_dns    = ['50', '50']
+  setid         = ['cde-blue-tv2', 'cde-green-tv2']
+  weight_record = ['cde-blue-ext-v2.razorpay.com', 'cde-green-ext-v2.razorpay.com']
+}
+module "my-app-int" {
+  source        = "../../modules/weighted-dns/"
+  zone_id       = data.aws_route53_zone.com.id
+  record_name   = "exp-int"
+  record_type   = "CNAME"
+  env           = "prod"
+  ttl           = "300"
+  target_record = "prod-exp-int-weighted.rzp.com"
+  weight_dns    = ['50', '50']
+  setid         = ['prod-green-tv2', 'prod-white-tv2']
+  weight_record = ['prod-green-int-v2.razorpay.com', 'prod-white-int-v2.razorpay.com']
+}
